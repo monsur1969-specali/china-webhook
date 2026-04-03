51394b10cd75bad18a85c76f18f31f23c7ca544b
monsur1969-specali <monsur1969@gmail.com>
2026-04-04 00:48:17 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-require('dotenv').config();require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
